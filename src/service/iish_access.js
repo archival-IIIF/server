292c250afd6f44cc7fa62a0bc71e5f31eb0cc5fa
kerim1 <kerim1@users.noreply.github.com>
2018-12-14 12:19:13 +0100

    Update access for IISH

--- src/service/iish_access.js
+++ src/service/iish_access.js
@@ -1,35 +1,50 @@
 const {AccessState, isIpInRange, checkTokenDb} = require('../lib/Security');
 
 async function hasAccess({item, ip, identities}) {
-    return {state: AccessState.CLOSED};
-
-     //if (item.collection_id !== 'test')
-         return {state: AccessState.OPEN};
-
     if (item.collection_id === null)
-         return {state: AccessState.OPEN};
+        return {state: AccessState.OPEN};
 
-    if (!ip && (!identities || (identities.length === 0)))
-        return {state: AccessState.CLOSED};
+    const accessCode = (item.iish && item.iish.access) ? item.iish.access : 'open';
+    if (accessCode === 'open')
+        return {state: AccessState.OPEN};
 
-    if (!isIpInRange(ip))
-        return {state: AccessState.CLOSED};
-
-    if (identities && identities.length > 0) {
-        const tokensInfo = await checkTokenDb(identities);
-        const tokenInfo = tokensInfo.find(tokenInfo => tokenInfo.collection_id === item.collection_id);
-        const hasToken = tokenInfo !== undefined;
-
-        if (hasToken && (item.type === 'image'))
-            return {state: AccessState.TIERED, tier: {name: 'pictoright', maxSize: 450}};
-
-        if (hasToken)
+    if (accessCode === 'eadRestricted') {
+        if (isIpInRange(ip) || await hasToken(item, identities))
             return {state: AccessState.OPEN};
 
         return {state: AccessState.CLOSED};
     }
 
+    if (accessCode === 'restricted') {
+        if (isIpInRange(ip) || await hasToken(item, identities))
+            return {state: AccessState.OPEN};
+
+        if (item.type === 'image')
+            return {state: AccessState.TIERED, tier: {name: accessCode, maxSize: 1500}};
+
+        return {state: AccessState.CLOSED};
+    }
+
+    if (accessCode === 'minimal' || accessCode === 'pictoright') {
+        if (isIpInRange(ip) || await hasToken(item, identities))
+            return {state: AccessState.OPEN};
+
+        if (item.type === 'image')
+            return {state: AccessState.TIERED, tier: {name: accessCode, maxSize: 450}};
+
+        return {state: AccessState.CLOSED};
+    }
+
+    if (isIpInRange(ip) || await hasToken(item, identities))
+        return {state: AccessState.OPEN};
+
     return {state: AccessState.CLOSED};
+}
+
+async function hasToken(item, identities) {
+    const tokensInfo = await checkTokenDb(identities);
+    const tokenInfo = tokensInfo.find(tokenInfo => tokenInfo.collection_id === item.collection_id);
+    return tokenInfo !== undefined;
 }
 
 module.exports = hasAccess;
