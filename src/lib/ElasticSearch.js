4758a8d21b2decf9babbf6cf3e7846e2bb49ae20
kerim1 <kerim1@users.noreply.github.com>
2019-01-18 08:42:43 +0100

    Repeatedly ping ES until it succeeds and then indexes and mapping are created

--- src/lib/ElasticSearch.js
+++ src/lib/ElasticSearch.js
@@ -1,181 +1,194 @@
 const elasticsearch = require('elasticsearch');
 const config = require('./Config');
+const logger = require('./Logger');
 
 const client = new elasticsearch.Client({
     host: config.elasticSearchUrl,
     apiVersion: '6.5'
 });
 
-setTimeout(() => {
-    client.indices.exists({index: 'items'}).then(exists => {
-        if (exists) return;
+setMapping();
 
-        client.indices.create({
-            index: 'items',
-            body: {
-                mappings: {
-                    '_doc': {
-                        properties: {
-                            id: {
-                                type: 'keyword'
-                            },
-                            parent_id: {
-                                type: 'keyword'
-                            },
-                            collection_id: {
-                                type: 'keyword'
-                            },
-                            metadata_id: {
-                                type: 'keyword'
-                            },
-                            type: {
-                                type: 'keyword'
-                            },
-                            label: {
-                                type: 'text',
-                                fielddata: true
-                            },
-                            description: {
-                                type: 'text'
-                            },
-                            authors: {
-                                properties: {
-                                    type: {
-                                        type: 'keyword'
-                                    },
-                                    name: {
-                                        type: 'text'
+async function setMapping() {
+    try {
+        await client.ping();
+
+        const itemsExists = await client.indices.exists({index: 'items'});
+        if (!itemsExists) {
+            await client.indices.create({
+                index: 'items',
+                body: {
+                    mappings: {
+                        '_doc': {
+                            properties: {
+                                id: {
+                                    type: 'keyword'
+                                },
+                                parent_id: {
+                                    type: 'keyword'
+                                },
+                                collection_id: {
+                                    type: 'keyword'
+                                },
+                                metadata_id: {
+                                    type: 'keyword'
+                                },
+                                type: {
+                                    type: 'keyword'
+                                },
+                                label: {
+                                    type: 'text',
+                                    fielddata: true
+                                },
+                                description: {
+                                    type: 'text'
+                                },
+                                authors: {
+                                    properties: {
+                                        type: {
+                                            type: 'keyword'
+                                        },
+                                        name: {
+                                            type: 'text'
+                                        }
                                     }
-                                }
-                            },
-                            dates: {
-                                type: 'keyword'
-                            },
-                            physical: {
-                                type: 'keyword'
-                            },
-                            size: {
-                                type: 'long'
-                            },
-                            order: {
-                                type: 'short',
-                            },
-                            created_at: {
-                                type: 'date'
-                            },
-                            width: {
-                                type: 'short'
-                            },
-                            height: {
-                                type: 'short'
-                            },
-                            resolution: {
-                                type: 'short'
-                            },
-                            duration: {
-                                type: 'short'
-                            },
-                            metadata: {
-                                properties: {
-                                    label: {
-                                        type: 'keyword'
-                                    },
-                                    value: {
-                                        type: 'keyword'
+                                },
+                                dates: {
+                                    type: 'keyword'
+                                },
+                                physical: {
+                                    type: 'keyword'
+                                },
+                                size: {
+                                    type: 'long'
+                                },
+                                order: {
+                                    type: 'short',
+                                },
+                                created_at: {
+                                    type: 'date'
+                                },
+                                width: {
+                                    type: 'short'
+                                },
+                                height: {
+                                    type: 'short'
+                                },
+                                resolution: {
+                                    type: 'short'
+                                },
+                                duration: {
+                                    type: 'short'
+                                },
+                                metadata: {
+                                    properties: {
+                                        label: {
+                                            type: 'keyword'
+                                        },
+                                        value: {
+                                            type: 'keyword'
+                                        }
                                     }
-                                }
-                            },
-                            original: {
-                                properties: {
-                                    uri: {
-                                        type: 'keyword'
-                                    },
-                                    puid: {
-                                        type: 'keyword'
+                                },
+                                original: {
+                                    properties: {
+                                        uri: {
+                                            type: 'keyword'
+                                        },
+                                        puid: {
+                                            type: 'keyword'
+                                        }
                                     }
-                                }
-                            },
-                            access: {
-                                properties: {
-                                    uri: {
-                                        type: 'keyword'
-                                    },
-                                    puid: {
-                                        type: 'keyword'
+                                },
+                                access: {
+                                    properties: {
+                                        uri: {
+                                            type: 'keyword'
+                                        },
+                                        puid: {
+                                            type: 'keyword'
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
-            }
-        });
-    });
+            });
 
-    client.indices.exists({index: 'texts'}).then(exists => {
-        if (exists) return;
+            logger.info("Created the index 'items' with a mapping");
+        }
 
-        client.indices.create({
-            index: 'texts',
-            body: {
-                mappings: {
-                    '_doc': {
-                        properties: {
-                            id: {
-                                type: 'keyword'
-                            },
-                            item_id: {
-                                type: 'keyword'
-                            },
-                            collection_id: {
-                                type: 'keyword'
-                            },
-                            type: {
-                                type: 'keyword'
-                            },
-                            language: {
-                                type: 'keyword'
-                            },
-                            uri: {
-                                type: 'keyword'
-                            },
-                            text: {
-                                type: 'text'
+        const textsExists = await client.indices.exists({index: 'texts'});
+        if (!textsExists) {
+            await client.indices.create({
+                index: 'texts',
+                body: {
+                    mappings: {
+                        '_doc': {
+                            properties: {
+                                id: {
+                                    type: 'keyword'
+                                },
+                                item_id: {
+                                    type: 'keyword'
+                                },
+                                collection_id: {
+                                    type: 'keyword'
+                                },
+                                type: {
+                                    type: 'keyword'
+                                },
+                                language: {
+                                    type: 'keyword'
+                                },
+                                uri: {
+                                    type: 'keyword'
+                                },
+                                text: {
+                                    type: 'text'
+                                }
                             }
                         }
                     }
                 }
-            }
-        });
-    });
+            });
 
-    client.indices.exists({index: 'tokens'}).then(exists => {
-        if (exists) return;
+            logger.info("Created the index 'texts' with a mapping");
+        }
 
-        client.indices.create({
-            index: 'tokens',
-            body: {
-                mappings: {
-                    '_doc': {
-                        properties: {
-                            token: {
-                                type: 'keyword'
-                            },
-                            collection_id: {
-                                type: 'keyword'
-                            },
-                            from: {
-                                type: 'date'
-                            },
-                            to: {
-                                type: 'date'
+        const tokensExists = await client.indices.exists({index: 'tokens'});
+        if (!tokensExists) {
+            await client.indices.create({
+                index: 'tokens',
+                body: {
+                    mappings: {
+                        '_doc': {
+                            properties: {
+                                token: {
+                                    type: 'keyword'
+                                },
+                                collection_id: {
+                                    type: 'keyword'
+                                },
+                                from: {
+                                    type: 'date'
+                                },
+                                to: {
+                                    type: 'date'
+                                }
                             }
                         }
                     }
                 }
-            }
-        });
-    });
-}, 5000);
+            });
+
+            logger.info("Created the index 'tokens' with a mapping");
+        }
+    }
+    catch (e) {
+        setMapping();
+    }
+}
 
 module.exports = client;
